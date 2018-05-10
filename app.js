00764fdcd188b8bc9d837638955a272f2c2c7675
danspeters <danpshop@gmail.com>
2018-05-10 15:40:11 +0200

    dialgue edit

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: null,
-    appPassword: null
+//    appId: null,
+//    appPassword: null
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
 });
 
 // Listen for messages from users 
